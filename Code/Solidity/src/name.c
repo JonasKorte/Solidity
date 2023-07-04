261eb0b18bdba4ebf98435de0b9f71c7aca34391
Delirium Technologies <59111455+JonasKorte@users.noreply.github.com>
2023-07-04 11:26:48 +0200

    Added USB Name to name.c

--- Code/Solidity/src/name.c
+++ Code/Solidity/src/name.c
@@ -1,1 +1,10 @@
 #include <usb_names.h>
+
+#define USB_NAME {'S','O','L','I','D','I','T','Y',' ','S','y','n','t','h'}
+#define USB_NAME_LEN 14
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+        2 + USB_NAME_LEN * 2,
+        3,
+        USB_NAME
+};
